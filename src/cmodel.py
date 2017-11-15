c90f4b9b655fa39f65fb2a6478512278e1aaef9e
scriptshi <xjtushilei@foxmail.com>
2017-11-15 21:01:04 +0800

    F

--- src/cmodel.py
+++ src/cmodel.py
@@ -94,7 +94,7 @@ class FindDoc:
             log.debug("老大进行第一轮的处理，分类器筛选掉诊断不了的病")
             words = self.process_sentences([choice_now])
             log.debug("老大分词结果:" +" ".join(words))
-            if gender == "male":
+            if gender == "M":
                 pred, prob = self.male_classifier.predict(" ".join(words))
                 if prob[0] > 0.9:
                     log.debug("分到科室：" + pred[0])
