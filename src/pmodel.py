f125193616390622bf7d12770b4485c9fec1de89
script <xjtushilei@foxmail.com>
2017-11-20 09:49:31 +0800

    jingwei的model，更新了平面距离，处理同义词

--- src/pmodel.py
+++ src/pmodel.py
@@ -9,7 +9,7 @@ class PredModel:
     #def __init__(self, seg_model_path="./model/cws.model", w2v_model_path="./model/model-wiki-hdf-5k.bin", dict_var_path="./val/dict_var.npy"):
     #def __init__(self, seg_model_path="./model/cws.model", w2v_model_path="./model/word2vec.model", dict_var_path="./val/dict_var.npy"):
 
-    def __init__(self, seg_model_path="./model/cws.model", w2v_model_path="./model/model-webqa-hdf-2c.bin", dict_var_path="./model/dict_var.npy"):
+    def __init__(self, seg_model_path="./model/cws.model", w2v_model_path="./model/model-webqa-hdf-2c.bin", dict_var_path="./val/dict_var.npy"):
         self.segmentor = Segmentor()
         self.segmentor.load(seg_model_path)
         self.ft = fastText.load_model(w2v_model_path)
@@ -18,6 +18,7 @@ class PredModel:
         self.prog = re.compile("[\.|\,|\?|\!|。|，|？|！]")
         self.wv_dim = 250
         self.name_weight = 0.25
+        self.th_word_mask =0.04
 
     def segment(self, sentence):
         words = self.segmentor.segment(sentence)
@@ -34,13 +35,14 @@ class PredModel:
             return vec
 
     def top_k(self, alist, k):
-        length = len(alist)
+        alist_tmp=alist
+        length = len(alist_tmp)
         if k <= 0 or k > length:
             return
         pos = []
         val = []
         for ii in range(k):
-            pos.append(np.where(alist == max(alist))[0][0])
+            pos.append(np.where(alist_tmp == max(alist_tmp))[0][0])
             val.append(alist[pos[ii]])
             alist[pos[ii]] = -10
 
@@ -85,33 +87,65 @@ class PredModel:
         mask_matrix = self.dict[7]
         mask_vec = self.dict[8]
 
+
         mask_layer = self.Masking(mask_matrix,age,gender)
 
 
         dim = self.wv_dim
         sent_vec = []
-
+        word_bag=[]
+        word_vec_bag=[]
         for chunk in self.split_into_chunks(input):
             chunk = chunk.strip()
             if len(chunk) == 0:
                 continue
             words = self.segment(chunk)
             chunk_wv = []
+            word_bag_tmp =[]
             for word in words:
                 wv = self.ft.get_word_vector(word)
                 #wv = self.ft[word]
                 chunk_wv.append(wv)
+                word_bag_tmp.append(word)
 
                 # if word in ['不','不是','没有','没','无']:
                 #     chunk_wv = []
                 #     break
 
             if len(chunk_wv) > 0:
-                sent_vec.append(self.unitvec(np.sum(chunk_wv, axis=0)))
+
+                #sent_vec.append(self.unitvec(np.sum(chunk_wv, axis=0)))
+
+                chunk_matrix = np.reshape(chunk_wv,[len(chunk_wv),dim])
+                chunk_sim = np.dot(chunk_matrix,symp_wv.T)
+                chunk_std = np.std(chunk_sim,axis = 1)
+
+                for jj in range(len(chunk_std)):
+                    if chunk_std[len(chunk_std)-1-jj]<self.th_word_mask:
+
+
+                        del word_bag_tmp[len(chunk_std)-1-jj]
+                        del chunk_wv[len(chunk_std)-1-jj]
+                if len(word_bag_tmp) > 0:
+                    word_bag.append(word_bag_tmp)
+                    word_vec_bag.append(chunk_wv)
+                    sent_vec.append(self.unitvec(np.sum(chunk_wv, axis=0)))
+
+
+
+
 
         if len(sent_vec) == 0:
-            print("something is wrong len(sent_vec) == 0")
+
+            print(input)
+            print('没有找到匹配的疾病，请具体描述您的症状')
             assert(False)
+
+
+
+        # for ii in range(len(word_vec_bag)):
+        #
+        #
 
         input_vec = self.unitvec(np.sum(sent_vec, axis=0))
         input_vec = np.reshape(input_vec, [1, dim])
@@ -119,7 +153,20 @@ class PredModel:
         name_dis = np.dot(input_vec, disease_name_vec.T)[0]
         combined_dis =(self.name_weight*name_dis+(1-self.name_weight)*symtom_dis)[0]*mask_layer*mask_vec[0]
         #combined_dis =(self.name_weight*name_dis+(1-self.name_weight)*symtom_dis)[0]
-        val, pos = self.top_k(combined_dis, k_disease)
+        val, pos = self.top_k(combined_dis, k_disease*2)
+
+    
+        # coeff_mask = np.zeros([1,len(pos)])
+        # coeff_mask[0][pos[0:10]] = 1
+        # neg_plane =  np.sum(symp_wv[pos[len(pos)-50:len(pos)]],axis =0)
+        # neg_plane = self.unitvec(neg_plane)
+        # neg_plane = np.reshape(neg_plane,[1,len(neg_plane)])
+        # coeff_shrink = 0.5+0.5*(1-np.dot(neg_plane,symp_wv.T))
+        # coeff_matrix = np.dot(coeff_shrink.T,np.ones([1,dim]))
+        # symp_wv_shrink = symp_wv*coeff_matrix
+        # symtom_dis = np.dot(input_vec, symp_wv_shrink.T)*coeff_mask
+        # combine_symtom_dis = symtom_dis[0]
+        # val, pos = self.top_k(combine_symtom_dis, k_disease)
 
 
 
@@ -181,4 +228,4 @@ class PredModel:
         symp_out_fin = np.array(symp_out_fin)[pos_simp]
 
 
-        return np.array(diseases)[pos], np.array(index)[pos],val,symp_out_fin, val_simp
+        return np.array(diseases)[pos], np.array(index)[pos],val,symp_out_fin, val_simp,word_bag
