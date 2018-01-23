c019fca87ad693a6bd7df10bd50c938f5e67b92c
scriptshi <xjtushilei@foxmail.com>
2018-01-23 12:57:05 +0800

    ner url修改

--- intelligent_triage/ner.py
+++ intelligent_triage/ner.py
@@ -1,7 +1,8 @@
 # coding=utf-8
 
-import requests
 import time
+
+import requests
 
 
 def post(content, userID="jerryz"):
@@ -12,15 +13,15 @@ def post(content, userID="jerryz"):
     }
     start_time = time.time()
     try:
-        resp = requests.post("http://walleai_nlu.ext.wsd.com/nlu", json=body, timeout=0.01).json()
+        resp = requests.post("http://172.27.0.6:8802/nlu", json=body, timeout=0.25).json()
         nerList = resp["reply"]["ner_norms"]
         slots = resp["reply"]["slots"]
         result = []
         for w in nerList:
-            result.extend(w.split("-"))
+            result.extend(w.split("|"))
         for l in slots.values():
             result.extend(l[0:2])
         result = list(set(result))
-        return result, resp, str(1000*(time.time() - start_time)).split('.')[0]
+        return result, resp, str(1000 * (time.time() - start_time)).split('.')[0]
     except Exception:
-        return [], "超时（250ms）或者json解析错误", str(1000*(time.time() - start_time)).split('.')[0]
+        return [], "超时（250ms）或者json解析错误", str(1000 * (time.time() - start_time)).split('.')[0]
