8b8fbd7cd460bbcdcb75c33b8baa8965a8a9c75f
script <xjtushilei@foxmail.com>
2018-01-31 21:54:46 +0800

    初步改造

--- intelligent_triage/server.py
+++ intelligent_triage/server.py
@@ -3,17 +3,19 @@ import json
 import logging.config
 import os
 import random
+import re
 import socket
 import sys
 import traceback
 from datetime import datetime
-from urllib.parse import urlparse, parse_qs
 
 from flask import Flask, jsonify
 from flask import request
 from flask_cors import CORS
 
+import ner
 from db_util import Mongo
+from dialogue import Dialogue
 from pmodel import PredModel
 
 app = Flask(__name__)
@@ -68,39 +70,49 @@ def unknow_error(error):
     return "内部错误", 500
 
 
-@app.route(CLIENT_API_PREDICT, methods=["GET"])
+@app.route(CLIENT_API_PREDICT, methods=["POST"])
 def predict():
-    query_params = parse_qs(urlparse(request.url).query)
+    post_data = request.get_json()
     # 记录请求日志
-    log = {"type": "get", "url": request.url, "time": datetime.utcnow(), "ip": request.remote_addr}
+    log = {"type": "get", "url": request.url, "time": datetime.utcnow(),
+           "ip": request.remote_addr, "post_data": post_data}
     mongo.info(log)
     # 检查url参数合法性
-    ok, res, code = predict_url_check(query_params)
+    ok, res, code = predict_data_check(post_data)
     if not ok:
         mongo.error({"type": "predict_url_check", "code": code, "res": res, "url": request.url,
-                     "time": datetime.utcnow(), "ip": request.remote_addr, "params": query_params})
+                     "time": datetime.utcnow(), "ip": request.remote_addr, "post_data": post_data})
         return jsonify(res), code
-
-    input = query_params["input"][0]
-    gender = query_params["gender"][0]
-    age = float(query_params["age"][0])
-    k_disease = int(query_params["k_disease"][0])
-    k_symptom = int(query_params["k_symptom"][0])
-    diseases, icd10, rate, symptoms, Coeff_sim_out, word_vec_bag = predictModel.predict(input=input,
-                                                                                        age=age,
-                                                                                        gender=gender,
-                                                                                        k_disease=k_disease,
-                                                                                        k_symptom=k_symptom)
-
+    diseases, icd10, rate, symptoms, Coeff_sim_out, vcb = predictModel.predict(input=post_data["all_choice"],
+                                                                               age=post_data["age"],
+                                                                               gender=post_data["gender"],
+                                                                               k_disease=post_data["k_disease"],
+                                                                               k_symptom=5)
     if diseases is None:
-        result = {"diseases": [], "icd10": [], "rate": [], "symptom": []}
+        result = {"diseases": [], "icd10": [], "rate": [], "recommendation_symtom": []}
     else:
         diseases = diseases.tolist()
         icd10 = icd10.tolist()
         symptoms = symptoms.tolist()
-        result = {"diseases": diseases, "icd10": icd10, "rate": rate, "symptom": symptoms}
+        # 疾病name,概率，icd10编号
+        disease_rate_list = []
+        for i, d in enumerate(diseases):
+            disease_rate_list.append([d, rate[i], icd10[i]])
+
+        # 用户输入过的东西分词后不再次推荐
+        no_use_symtom_list = process_sentences_sl(post_data["all_choice"])
+        # 用户没有选择过的选项不再次推荐
+        no_use_symtom_list.extend(post_data["all_choices"])
+        # ner识别出的同义词不再推荐
+        ner_words, resp, ner_time = ner.post(post_data["all_choice"], post_data["user_id"])
+        no_use_symtom_list.extend(ner_words)
+
+        recommendation_symtom = dialogue.core_method(disease_rate_list=disease_rate_list, input_list=symptoms,
+                                                     no_use_symtom_list=no_use_symtom_list, seqno=post_data["seqno"],
+                                                     max_recommend_sym_num=post_data["k_recommendation_symtom"])
+        result = {"diseases": diseases, "icd10": icd10, "rate": rate, "recommendation_symtom": recommendation_symtom}
     # 记录返回日志
-    log = {"type": "return", "params": query_params, "time": datetime.utcnow(),
+    log = {"type": "return", "post_data": post_data, "time": datetime.utcnow(),
            "ip": request.remote_addr, "url": request.url, "result": result}
     mongo.info(log)
     return jsonify(result)
@@ -113,37 +125,59 @@ def error(error_msg):
     }
 
 
-# 检查url参数合法性
-def predict_url_check(query_params):
-    if not ("sessionId" in query_params and "seqno" in query_params
-            and "input" in query_params and "userId" in query_params
-            and "k_disease" in query_params and "k_symptom" in query_params
-            and "age" in query_params and "gender" in query_params):
-        return False, error("错误的请求: url中没有包含sessionId或input或seqno或userId或age或gender或k_disease或k_symptom"), 400
-    gender = query_params["gender"][0]
+# 去掉停用词，并用空格替换
+def remove_stopwords(line):
+    sws = "[！|“|”|‘|’|…|′|｜|、|，|。|〈|〉:：|《|》|「|」|『|』|【|】|〔|〕|︿|！|＃|＄|％|＆|＇|（|）|＊|＋|－|,．||；|＜|＝|＞|？|＠|［|］|＿|｛|｜|｝|～|↑|→|≈|①|②|③|④|⑤|⑥|⑦|⑧|⑨|⑩|￥|Δ|Ψ|γ|μ|φ|!|\"|'|#|\$|%|&|\*|\+|,|\.|;|\?|\\\|@|\(|\)|\[|\]|\^|_|`|\||\{|\}|~|<|>|=]"
+    return re.sub(sws, " ", line)
+
+
+# 处理输入用的分词函数,包括ltp的分词和使用标点分词的结果
+def process_sentences_sl(sentences):
+    words = []
+    for sentence in sentences:
+        sent = remove_stopwords(sentence)
+        for word in segmentor.segment(sent):
+            words.append(word)
+        sent = remove_stopwords(sentence)
+        words.extend(sent.split(" "))
+    return words
+
+
+# 检查post参数合法性
+def predict_data_check(post_data):
+    # 检查数据是否为空
+    if post_data is None:
+        res = error("错误的请求: 无法解析JSON 或 请求中请设置 'content-type' 为 'application/json' ")
+        return False, res, 400
+    if not ("session_id" in post_data and "seqno" in post_data and "user_id" in post_data
+            and "k_disease" in post_data and "k_recommendation_symtom" in post_data
+            and "age" in post_data and "gender" in post_data
+            and "all_choice" in post_data and "all_choices" in post_data):
+        return False, error("错误的请求: json中缺少字段"), 400
+    gender = post_data["gender"]
     if not (gender == "male" or gender == "female"):
         return False, error("错误的请求: 错误的数据格式(gender格式不对,应该是male或female)"), 400
-    age = query_params["age"][0]
+    age = post_data["age"]
     try:
         age = float(age)
         if age < 0:
             return False, error("错误的请求: 错误的数据格式(age格式不对,应该是正整数或正浮点数)"), 400
     except ValueError:
         return False, error("错误的请求: 错误的数据格式(age格式不对,应该是正整数或正浮点数)"), 400
-    k_disease = query_params["k_disease"][0]
+    k_disease = post_data["k_disease"]
     try:
         k_disease = int(k_disease)
         if k_disease < 0:
             return False, error("错误的请求: 错误的数据格式(k_disease格式不对,应该是正整数)"), 400
     except ValueError:
         return False, error("错误的请求: 错误的数据格式(k_disease格式不对,应该是正整数)"), 400
-    k_symptom = query_params["k_symptom"][0]
+    k_recommendation_symtom = post_data["k_recommendation_symtom"]
     try:
-        k_symptom = int(k_symptom)
-        if k_symptom < 0:
-            return False, error("错误的请求: 错误的数据格式(k_symptom 格式不对,应该是正整数)"), 400
+        k_recommendation_symtom = int(k_recommendation_symtom)
+        if k_recommendation_symtom < 0:
+            return False, error("错误的请求: 错误的数据格式(k_recommendation_symtom 格式不对,应该是正整数)"), 400
     except ValueError:
-        return False, error("错误的请求: 错误的数据格式(k_symptom 格式不对,应该是正整数)"), 400
+        return False, error("错误的请求: 错误的数据格式(k_recommendation_symtom 格式不对,应该是正整数)"), 400
     return True, None, None
 
 
@@ -199,6 +233,12 @@ if __name__ == '__main__':
         pos_model_path=app_config["model_file"]["root_path"] + app_config["model_file"]["pos_model"],
         w2v_model_path=app_config["model_file"]["root_path"] + app_config["model_file"]["fasttext_model"],
         dict_var_path=app_config["model_file"]["root_path"] + app_config["model_file"]["dict_var"])
+    segmentor = predictModel.segmentor
+    dialogue = Dialogue(
+        disease_symptom_file_path=app_config["model_file"]["root_path"] + app_config["model_file"][
+            "disease_symptom_file_dir"],
+        all_symptom_count_file_path=app_config["model_file"]["root_path"] + app_config["model_file"][
+            "all_symptom_count_file_path"])
     endtime = datetime.now()
     print("模型加载一共用时:" + str((endtime - starttime).seconds) + "秒" + "\nfinished loading models.\n server started .")
     ###########################初始化mongodb驱动###########################
